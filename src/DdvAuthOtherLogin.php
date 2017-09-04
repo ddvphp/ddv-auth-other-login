82a4a982854874aaf299dd92bab562d9ee1af4ca
hua <yuchonghua@163.com>
2017-09-04 17:09:41 +0800

    Add some

--- src/DdvAuthOtherLogin.php
+++ src/DdvAuthOtherLogin.php
@@ -10,5 +10,75 @@ namespace DdvPhp;
  */
 class DdvAuthOtherLogin
 {
+    private static $config = array(
+        'wechat_mp'=>array(
 
+        ),
+        'wechat_web'=>array(
+
+        ),
+        'wechat_app'=>array(
+
+        ),
+        'alipay_web'=>array(
+
+        ),
+        'alipay_app'=>array(
+
+        )
+    );
+    /**
+     * 设置配置文件
+     * @param array $config [配置参数]
+     * @return DdvAuthOtherLogin $this [请求对象]
+     */
+    public static function setConfig($config = array(), $type=null) {
+        if (empty($type)&&is_null($type)){
+            if (isset($config)&&is_array($config)){
+                foreach ($config as $type => $c){
+                    self::setConfig($c, $type);
+                }
+            }
+        }else{
+            self::setConfigByType($config, $type);
+        }
+        return self::class;
+    }
+    public static function setConfigByType($config = array(), $type=null) {
+        $c = &self::$config;
+        switch (self::type2type($type)){
+            case 'wechat_mp':
+                $c['wechat_mp'] = $config;
+                break;
+            case 'wechat_web':
+                $c['wechat_web'] = $config;
+                break;
+            case 'wechat_app':
+                $c['wechat_app'] = $config;
+                break;
+            case 'alipay_web':
+                $c['alipay_web'] = $config;
+                break;
+            case 'alipay_app':
+                $c['alipay_app'] = $config;
+                break;
+        }
+    }
+    protected static function type2type($type){
+        switch ($type){
+            case 'weixin_mp':
+                $type = 'wechat_mp';
+                break;
+            case 'weixin_web':
+                $type = 'wechat_web';
+                break;
+            case 'weixin_app':
+                $type = 'wechat_app';
+                break;
+        }
+        return $type;
+    }
+    public static function getLoginInfo(){
+        return self::$config;
+    }
 }
