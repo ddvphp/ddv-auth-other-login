d392712c4f7f803c3fc767f22c2caf0420c6ea67
hua <yuchonghua@163.com>
2017-09-04 17:44:49 +0800

    Fixbug DdvAuthOtherLogin

--- src/DdvAuthOtherLogin.php
+++ src/DdvAuthOtherLogin.php
@@ -1,5 +1,6 @@
 <?php
 namespace DdvPhp;
+use DdvPhp\DdvAuthOtherLogin\Exception;
 
 /**
  * Class Cors
@@ -29,8 +30,20 @@ class DdvAuthOtherLogin
     );
     /**
      * 设置配置文件
+     * @param array $params [请求参数]
+     * @return array $r [返回配置参数]
+     */
+    public static function getLoginInfo($params = array()){
+        if (empty($params)){
+            $params = self::getParams();
+        }
+        // 判断是否支持该配置
+        return self::isHasType(empty($params['type'])?'':$params['type'])::getLoginInfo($params, self::$config);
+    }
+    /**
+     * 设置配置文件
      * @param array $config [配置参数]
-     * @return DdvAuthOtherLogin $this [请求对象]
+     * @return DdvAuthOtherLogin self [请求对象]
      */
     public static function setConfig($config = array(), $type=null) {
         if (empty($type)&&is_null($type)){
@@ -44,26 +57,61 @@ class DdvAuthOtherLogin
         }
         return self::class;
     }
+    /**
+     * 设置配置文件
+     * @param array $config [配置参数]
+     * @param string $type [参数类型]
+     * @return DdvAuthOtherLogin self [请求对象]
+     */
     public static function setConfigByType($config = array(), $type=null) {
-        $c = &self::$config;
-        switch (self::type2type($type)){
-            case 'wechat_mp':
-                $c['wechat_mp'] = $config;
-                break;
-            case 'wechat_web':
-                $c['wechat_web'] = $config;
-                break;
-            case 'wechat_app':
-                $c['wechat_app'] = $config;
-                break;
-            case 'alipay_web':
-                $c['alipay_web'] = $config;
-                break;
-            case 'alipay_app':
-                $c['alipay_app'] = $config;
-                break;
-        }
+        // 转换类型
+        $type = self::type2type($type);
+        // 判断是否支持该配置
+        self::isHasType($type);
+        // 设置配置信息
+        self::$config[$type] = $config;
+        // 返回类名
+        return self::class;
     }
+    /**
+     * 返回参数
+     * @return array $config [参数]
+     */
+    public static function getParams(){
+        $params = array();
+        if ((!empty($_POST))&&is_array($_POST)){
+            $params = array_merge($params, $_POST);
+        }
+        if ((!empty($_GET))&&is_array($_GET)){
+            $params = array_merge($params, $_GET);
+        }
+        return $params;
+    }
+    /**
+     * 设置配置文件
+     * @param string $type [参数类型]
+     */
+    public static function isHasType($type = null){
+        if (empty($type)){
+            throw new Exception('没有找到该类型', 'NOT_FIND_TYPE');
+        }
+        $className = '\\DdvPhp\\DdvAuthOtherLogin\\Lib\\'.ucfirst(preg_replace_callback(
+            '(\_\w)',
+            function ($matches) {
+                return strtoupper(substr($matches[0], 1));
+            },
+            (string)$type
+        ));;
+        if (!class_exists($className)) {
+            throw new Exception('没有找到该类型', 'NOT_FIND_TYPE');
+        }
+        return $className;
+    }
+    /**
+     * 设置配置文件
+     * @param string $type [参数类型]
+     * @return string $type [参数类型]
+     */
     protected static function type2type($type){
         switch ($type){
             case 'weixin_mp':
@@ -77,8 +125,5 @@ class DdvAuthOtherLogin
                 break;
         }
         return $type;
-    }
-    public static function getLoginInfo(){
-        return self::$config;
     }
 }
---
+++ src/DdvAuthOtherLogin/Lib/AlipayWeb.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hua
+ * Date: 2017/9/4
+ * Time: 下午5:33
+ */
+
+namespace DdvPhp\DdvAuthOtherLogin\Lib;
+
+
+class AlipayWeb
+{
+    public static function getLoginInfo(){
+        return ['3333'];
+
+    }
+
+}
